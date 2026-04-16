70c3e2e72c0a155dff66c985f954ea443c78ccd5
VIVIANXJ <105621463+VIVIANXJ@users.noreply.github.com>
2026-04-16 12:05:35 +1000

    Delete 2saportal-main directory

--- 2saportal-main/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Allow server-side calls between API routes
-  experimental: {},
-}
-
-module.exports = nextConfig
